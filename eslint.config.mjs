b2b23a826aabba919c9fa60486a920dd9355bd33
Muliak Dmytro <dmitriymuliak@gmail.com>
2025-09-26 14:53:21 +0300

    try commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,30 @@
 import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    ignores: [], // Need specify absolute path from root
   },
-];
+  {
+    rules: {
+      'no-unused-vars': 'off', // Note: you must disable the base rule as it can report incorrect errors
+      '@typescript-eslint/no-unused-vars': 'error'
+    },
+  },
+
+]);
 
 export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 const a;
 
-export default function RootLayout({
+export default function           RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
