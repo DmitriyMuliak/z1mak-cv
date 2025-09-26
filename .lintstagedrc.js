75737ecb3e3fc007df8b9efba27044444c0eb10e
Muliak Dmytro <dmitriymuliak@gmail.com>
2025-09-26 13:06:23 +0300

    add prettier & format files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,13 @@
-import path from 'path'
- 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
- 
+import path from 'path';
+
+const buildEslintCommand = (filenames) => {
+  return   `next lint --fix --file ${filenames
+  .map((f) => path.relative(process.cwd(), f))
+  .join(' --file ')}`;
+}
+
 const lintStagedConfig = {
   '*.{js,jsx,ts,tsx,mjs,json}': [buildEslintCommand],
-}
+};
 
 export default lintStagedConfig;
