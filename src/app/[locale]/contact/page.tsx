acdad798bd3478e5c2c5c28813c84cf7a8d51dbf
Dmitriy Muliak <dmitriymuliak@gmail.com>
2026-01-15 19:44:07 +0200

    [REFACTOR]: remove unused code

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -7,30 +7,6 @@ export default async function ContactPage(props: PageProps) {
   const { locale } = await props.params;
   const t = await getTranslations({ locale, namespace: 'pages.contact' });
 
-  // return (
-  //   <div className="flex justify-center w-full sm:p-6 md:p-8">
-  //     <div className="w-full max-w-4xl mx-auto">
-  //       <h2 className="font-heading text-3xl font-semibold mb-4 text-center">{t('title')}</h2>
-  //       <div className="flex justify-center items-center">
-  //         <ContactForm />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
-  // return (
-  //   <div className="flex-1 flex flex-col items-center justify-center w-full">
-  //     <div className="flex justify-center w-full pb-4 sm:p-6 md:p-8">
-  //       <div className="w-full max-w-4xl mx-auto">
-  //         <h2 className="font-heading text-3xl font-semibold mb-4 text-center">{t('title')}</h2>
-  //         <div className="flex justify-center items-center">
-  //           <ContactForm />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   return (
     <div className="flex flex-col w-full pb-4 sm:p-6 md:p-8">
       <div className="w-full max-w-4xl mx-auto">
