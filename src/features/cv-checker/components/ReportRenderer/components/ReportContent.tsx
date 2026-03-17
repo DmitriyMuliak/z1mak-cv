af85820c86bdead82155f7ae804c2e791b068752
Dmytro <dmitriymuliak@gmail.com>
2026-03-17 12:51:35 +0200

    Add report animations (#59)
    * [ADD]: report part animations
    
    * [ADD]: cloude settings
    
    * [ADD]: "cloude settings"
    
    This reverts commit f79ce5815b5236457b8f7d8c7fe62d8296b87553.

--- src/features/cv-checker/components/ReportRenderer/components/ReportContent.tsx
+++ src/features/cv-checker/components/ReportRenderer/components/ReportContent.tsx
@@ -6,23 +6,24 @@ import { SECTION_COMPONENTS } from '../config';
 
 interface Props {
   report: AnalysisSchemaType;
-  jobId: string | null;
 }
 
-export const ReportContent: React.FC<Props> = ({ report, jobId }) => {
+export const ReportContent: React.FC<Props> = ({ report }) => {
   const activeSections = useMemo(() => {
     const service = new SchemaService(report);
     return service.getUiSections();
   }, [report]);
 
   return (
-    <AnimationContainer id={`${jobId}:result`}>
-      <div className="space-y-6 w-full">
-        {activeSections.map((sectionKey) => {
-          const Component = SECTION_COMPONENTS[sectionKey];
-          return <Component key={sectionKey} data={report} />;
-        })}
-      </div>
-    </AnimationContainer>
+    <div className="space-y-6 w-full">
+      {activeSections.map((sectionKey) => {
+        const Component = SECTION_COMPONENTS[sectionKey];
+        return (
+          <AnimationContainer key={sectionKey} id={sectionKey}>
+            <Component data={report} />
+          </AnimationContainer>
+        );
+      })}
+    </div>
   );
 };
